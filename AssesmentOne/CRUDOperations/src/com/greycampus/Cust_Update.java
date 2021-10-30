1e63c4160e73545e71b36883d6666380de1a0c39
rahul-1633 <61259062+rahul-1633@users.noreply.github.com>
2021-10-30 14:33:02 +0530

    Update Cust_Update.java
    Sorry as I forgot to save this file so updating now.

--- AssesmentOne/CRUDOperations/src/com/greycampus/Cust_Update.java
+++ AssesmentOne/CRUDOperations/src/com/greycampus/Cust_Update.java
@@ -1,5 +1,48 @@
 package com.greycampus;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Cust_Update {
+
+	public static java.sql.Date getCurrentDate() {
+		java.util.Date today = new java.util.Date();
+		return new java.sql.Date(today.getTime());
+	}
+
+	public static void main(String[] args) {
+		Connection connection = null;
+		PreparedStatement prepareStatement = null;
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/new_schema","root","root");
+			prepareStatement = connection.prepareStatement("update customer_details set street = ?, updated_by = ? , updated_on = ? where cid = ?");
+			prepareStatement.setString(1, "Gali no: 44");
+			prepareStatement.setString(2, "Siddharth");
+			prepareStatement.setDate(3, getCurrentDate());
+			prepareStatement.setInt(4, 1);
+			prepareStatement.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} 
+		
+		finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+			
+			try {
+				prepareStatement.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
 
 }
